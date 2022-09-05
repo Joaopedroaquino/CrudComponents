79bc5b6e46b4d131c44de81ef6aec6112c158b3e
joaopedroaquino <aqerhack@gmail.com>
2022-09-05 13:55:52 -0300

    adicionando a tabbar

---
+++ lib/src/widgets/tabbar/tab_bar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class TabBarWidget extends StatefulWidget {
+  const TabBarWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TabBarWidgetData();
+  }
+}
+
+class TabBarWidgetData extends State<TabBarWidget>
+    with TickerProviderStateMixin {
+  late TabController _tabControllerList;
+
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabControllerList = TabController(length: 2, vsync: this, initialIndex: 0);
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabControllerList.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Colors.white30,
+            child: TabBar(
+              indicatorColor: Colors.black,
+              labelColor: Colors.black,
+              labelStyle: const TextStyle(),
+              controller: _tabControllerList,
+              tabs: [
+                ///TAB DE LISTAGEM
+                Tab(
+                  child: Row(
+                    children: const [
+                      Icon(Icons.people_alt_outlined),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Listagem"),
+                    ],
+                  ),
+                ),
+
+                ///TAB DE DETALHE
+                Tab(
+                  child: Row(
+                    children: const [
+                      Icon(Icons.info),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Detalhe"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabControllerList,
+              children: const [
+                Card(
+                  child: Text('data'),
+                ),
+                Center(
+                  child: Card(
+                    child: Text('data'),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          //LISTVIEW DA PRIMEIRA TAB
+          Expanded(
+            child: TabBarView(
+              controller: _tabControllerList,
+              children: const [Center()],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
